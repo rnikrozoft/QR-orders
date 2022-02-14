b7eeb71a687b6940faeeadb6868eb08094704a85
rnikrozoft <rnikrozoft.dev@gmail.com>
2022-02-15 00:15:55 +0700

    enable helper

---
+++ application/controllers/Admins.php
@@ -0,0 +1,29 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Admins extends CI_Controller
+{
+
+    /**
+     * Index Page for this controller.
+     *
+     * Maps to the following URL
+     * 		http://example.com/index.php/welcome
+     *	- or -
+     * 		http://example.com/index.php/welcome/index
+     *	- or -
+     * Since this controller is set as the default controller in
+     * config/routes.php, it's displayed at http://example.com/
+     *
+     * So any other public methods not prefixed with an underscore will
+     * map to /index.php/welcome/<method_name>
+     * @see https://codeigniter.com/user_guide/general/urls.html
+     */
+    public function index()
+    {
+        $data['scripts'][] = '/assets/js/plugins/chartjs.min.js';
+        $data['scripts'][] = '/assets/js/custom/dataset1.js';
+
+        $this->load->view('admins_area/dashboard', $data);
+    }
+}
---
+++ application/views/templates/header.php
@@ -0,0 +1,42 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+?>
+<!--
+=========================================================
+* Argon Dashboard 2 - v2.0.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-dashboard
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://www.creative-tim.com/license)
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+-->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="apple-touch-icon" sizes="76x76" href="https://demos.creative-tim.com/argon-dashboard//assets/img/apple-icon.png">
+    <link rel="icon" type="image/png" href="https://demos.creative-tim.com/argon-dashboard//assets/img/favicon.png">
+    <title>
+        Argon Dashboard 2 by Creative Tim
+    </title>
+
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+
+    <!-- Nucleo Icons -->
+    <link href="<?php echo base_url('/assets/css/nucleo-icons.css') ?>" rel="stylesheet" />
+    <link href="<?php echo base_url('/assets/css/nucleo-svg.css') ?>" rel="stylesheet" />
+
+    <!-- Font Awesome Icons -->
+    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+
+    <!-- CSS Files -->
+    <link id="pagestyle" href="<?php echo base_url('/assets/css/argon-dashboard.min.css') ?>" rel="stylesheet" />
+    <!-- Anti-flicker snippet (recommended)  -->
+</head>
