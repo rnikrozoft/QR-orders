a4d874508c5a8df330152abd23e240d3131b3c91
rnikrozoft <rnikrozoft.dev@gmail.com>
2022-02-21 17:51:36 +0700

    create index method

--- application/controllers/Foods.php
+++ application/controllers/Foods.php
@@ -12,40 +12,72 @@ class Foods extends CI_Controller
 
     public function index()
     {
-        // $data['styles'][] = 'https://cdn.datatables.net/1.11.4/css/dataTables.bootstrap4.min.css';
-        // $data['scripts'][] = 'https://code.jquery.com/jquery-3.5.1.js';
-        // $data['scripts'][] = 'https://cdn.datatables.net/1.11.4/js/jquery.dataTables.min.js';
-        // $data['scripts'][] = 'https://cdn.datatables.net/1.11.4/js/dataTables.bootstrap4.min.js';
+        $data['styles'][] = 'https://cdn.datatables.net/1.11.4/css/dataTables.bootstrap4.min.css';
         $data['styles'][] = base_url('/assets/css/custom/food.css');
-
+        $data['scripts'][] = 'https://code.jquery.com/jquery-3.5.1.js';
+        $data['scripts'][] = 'https://cdn.datatables.net/1.11.4/js/jquery.dataTables.min.js';
+        $data['scripts'][] = 'https://cdn.datatables.net/1.11.4/js/dataTables.bootstrap4.min.js';
+        $data['scripts'][] = base_url('/assets/js/custom/datatable.js');
+        $data['scripts'][] = base_url('/assets/js/custom/food.js');
 
         $data['data'] = $this->Food_model->getAllData();
-        $this->load->view('admins_area/foods',$data);
+        $this->load->view('admins_area/foods', $data);
     }
 
     public function insert()
     {
-        if (!$this->Employee_model->insert_entry()) {
+        $config['upload_path']          = './assets/uploads/foods/';
+        $config['allowed_types']        = 'jpg|png';
+        $config['encrypt_name']        = true;
+        $this->upload->initialize($config);
+
+        if (!$this->upload->do_upload('i')) {
+            $error = array('error' => $this->upload->display_errors());
             $this->session->set_flashdata('message', 'มีข้อผิดพลาดเกิดขึ้น');
+        } else {
+            $data = array('upload_data' => $this->upload->data());
+            $this->Food_model->insert_entry($data['upload_data']['file_name']);
         }
-        redirect(base_url('/employees'));
+        redirect(base_url('/foods'));
     }
 
     public function deleteAll()
     {
-        $this->Employee_model->deleteAll();
-        redirect(base_url('/employees'));
+        $this->Food_model->deleteAll();
+
+        $path = './assets/uploads/foods/*';
+        foreach ($path as $file) {
+            if (is_file($file)) {
+                unlink($file);
+            }
+        }
+        delete_files('./assets/uploads/foods/', TRUE);
+        redirect(base_url('/foods'));
     }
 
-    public function deleteByID($id)
+    public function deleteByID($id, $img)
     {
-        $this->Employee_model->deleteByID($id);
-        redirect(base_url('/employees'));
+        unlink('./assets/uploads/foods/' . $img);
+        $this->Food_model->deleteByID($id);
+        redirect(base_url('/foods'));
     }
 
     public function edit()
     {
-        $this->Employee_model->update_entry($_POST);
-        redirect(base_url('/employees'));
+        $config['upload_path']          = './assets/uploads/foods/';
+        $config['allowed_types']        = 'jpg|png';
+        $config['encrypt_name']        = true;
+        $this->upload->initialize($config);
+
+        if (!$this->upload->do_upload('iup')) {
+            $img = $_POST['img'];
+        } else {
+            $data = array('upload_data' => $this->upload->data());
+            $img = $data['upload_data']['file_name'];
+            unlink($config['upload_path'].$_POST['img']);
+        }
+
+        $this->Food_model->update_entry($img);
+        redirect(base_url('/foods'));
     }
 }
